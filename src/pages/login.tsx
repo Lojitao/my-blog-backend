4fa78bfac1b50331208faa7d147b5ed10be7e297
Lojitao <Lojitao@outlook.com>
2025-01-17 10:38:48 +0800

    [Update]登入畫面完成

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,19 +1,44 @@
-import React from 'react';
-import { Button, DatePicker } from 'antd';
+import React, { useState } from 'react';
+import { Button, Input } from 'antd';
+import { UserOutlined,EyeOutlined,EyeInvisibleOutlined } from '@ant-design/icons';
 
 export const LoginRoute: React.FC = () => {
+  const [account,setAccount] = useState("")
+  const [secret,setSecret] = useState("")
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false); // 控制圖標狀態
+
+  function iconChange(){
+    setIsPasswordVisible((prev) => !prev); // 切換圖標狀態
+  }
+
+  function submit(){
+    console.log('account',account);
+    console.log('secret',secret);
+  }
+
   return (
-    <div style={{ padding: 24 }}>
-      <DatePicker className='block' />
-      <Button className="text-2xl" type="primary">登入</Button>
-      <p className="text-2xl bg-black">asdsa</p>
+    <div className='w-fit h-screen m-auto flex flex-col justify-center items-center'>
+      <Input className='w-fit flex mb-2'
+        value={account} onChange={(e) => setAccount(e.target.value)}
+        placeholder="帳號" prefix={<UserOutlined />}
+      />
+
+      <Input className='w-fit flex mb-2'
+        value={secret} onChange={(e) => setSecret(e.target.value)}
+        placeholder="密碼" 
+        type={isPasswordVisible ? "text" : "password"} // 切換密碼輸入顯示類型
+        prefix={
+          isPasswordVisible ? (
+            <EyeInvisibleOutlined onClick={iconChange} style={{ cursor: 'pointer' }} />
+          ) : (
+            <EyeOutlined onClick={iconChange} style={{ cursor: 'pointer' }} />
+          )
+        }
+      />
+      
+      <Button onClick={submit} type="primary">登入</Button>
+
     </div>
   );
 };
 
-
-// export const LoginRoute = () => {
-//   return (
-//     <p>登入頁拉!!!</p>
-//   );
-// };
